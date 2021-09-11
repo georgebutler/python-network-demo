1630df7c12cc6b62e8a02317e9179d4a89ecff1b
George Butler <georgebutleriv@gmail.com>
2021-09-11 13:52:47 -0700

    Server message

--- src/server.py
+++ src/server.py
@@ -6,9 +6,11 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("127.0.0.1", 32007))
 server.listen()
 
+print("Server now listening for connections.")
+
 
 def threaded_client(connection, address):
-    print("New Client Connected: ", address)
+    print("New client connected: ", address)
     connection.send("Hello from server!".encode())
 
 
