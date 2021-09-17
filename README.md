ef9ce2c4ffea5647a2b65a6dcde1d39a91a55685
George Butler <georgebutleriv@gmail.com>
2021-09-17 14:51:23 -0700

    Server

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Python Network Demo
 ## Network Demo for CSC698
+
+## Server
+
+* export FLASK_APP=src/server.py
+* export FLASK_ENV=development
+* python -m flask run
+
+## Important Notes
+
+This networking demo is purely for educational purposes. There have been no security features implemented, so do not use it for production applications.
--- src/server.py
+++ src/server.py
@@ -1,21 +1,103 @@
-import socket
-import pickle
-import threading
+from flask import Flask, request, jsonify, make_response
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("127.0.0.1", 32007))
-server.listen()
+app = Flask(__name__)
 
-print("Server now listening for connections.")
+rooms = {}
+users = {}
 
-
-def threaded_client(connection, address):
-    print("New client connected: ", address)
-    connection.send("Hello from server!".encode())
+# Index
 
 
-while True:
-    conn, addr = server.accept()
+@app.route('/')
+def index():
+    return make_response(jsonify({
+        "success": True
+    }), 200)
 
-    thread = threading.Thread(target=threaded_client, args=(conn, addr))
-    thread.start()
+
+# Users
+
+
+# List all the users
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    return make_response(jsonify(users), 200)
+
+# Create a user with a given name
+
+
+@app.route('/users/<name>', methods=['POST'])
+def create_user(name):
+    if (len(name) >= 3 and len(name) <= 16):
+        if name in users.keys():
+            return make_response(jsonify({
+                "success": False,
+                "error": "User name is taken."
+            }), 200)
+        else:
+            users[name] = {
+                "name": name,
+                "current_room": ""
+            }
+
+            return make_response(jsonify({
+                "success": True,
+                "user": users[name]
+            }), 200)
+    else:
+        return make_response(jsonify({
+            "success": False,
+            "error": "User name must be between 3 and 16 characters."
+        }), 400)
+
+
+# Rooms
+
+
+# List all the rooms
+@app.route('/rooms', methods=['GET'])
+def get_rooms():
+    return make_response(jsonify(rooms), 200)
+
+
+# Create a room with a given name
+@app.route('/rooms/<name>', methods=['POST'])
+def create_room(name):
+    if (len(name) >= 3 and len(name) <= 16):
+        if name in rooms.keys():
+            return make_response(jsonify({
+                "success": False,
+                "error": "Room name is taken."
+            }), 200)
+        else:
+            rooms[name] = {
+                "name": name,
+                "users": {}
+            }
+
+            return make_response(jsonify({
+                "success": True,
+                "room": rooms[name]
+            }), 200)
+    else:
+        return make_response(jsonify({
+            "success": False,
+            "error": "Room name must be between 3 and 16 characters."
+        }), 400)
+
+
+# Joining
+
+
+@app.route('/join/<name>', methods=['POST'])
+def join_room(name):
+    data = request.json
+    return make_response(jsonify(data), 200)
+
+
+@app.route('/leave/<name>', methods=['POST'])
+def leave_room(name):
+    print("Leaving room named: " + name)
+    return make_response(jsonify({}), 200)
